5cb8a54c4a581ef86bb8f011ff36e3153cc50391
Kevin Burke <kevin@burke.dev>
2022-05-19 15:41:46 -0700

    bigtext: switch from terminal-notifier
    In Mac 12.1 users need to manually grant access to terminal-notifier
    in "Accessibility" which is too high of a hurdle. Just send the
    notification using Applescript. We can't use as many options but
    that's fine.
    
    https://github.com/julienXX/terminal-notifier/issues/292

--- example/testit.go
+++ example/testit.go
@@ -8,9 +8,7 @@ import (
 
 func main() {
 	d := bigtext.Client{
-		Name:    "bigtext",
-		LogoURL: "http://icons.iconarchive.com/icons/xenatt/the-circle/512/App-Terminal-icon.png",
-		OpenURL: "https://google.com",
+		Name: "bigtext",
 	}
 	log.Fatal(d.Display("why hello there"))
 }
